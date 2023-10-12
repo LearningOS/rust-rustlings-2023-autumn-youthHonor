1b57486324f9b877c4bb77c185c0e29ce5411417
merak <gh.soul@qq.com>
2023-10-12 22:15:38 +0800

    second

--- exercises/primitive_types/primitive_types3.rs
+++ exercises/primitive_types/primitive_types3.rs
@@ -5,11 +5,11 @@
 // Execute `rustlings hint primitive_types3` or use the `hint` watch subcommand
 // for a hint.
 
-// I AM NOT DONE
 
 fn main() {
-    let a = ???
+    let a = vec![10;200];
 
+    println!("{:?}",a);
     if a.len() >= 100 {
         println!("Wow, that's a big array!");
     } else {
